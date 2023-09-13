0a90897e25b4f343a88e763963a586ea535ac122
Anuj Garg <anujgarg3366@gmail.com>
2023-09-13 20:53:05 +0530

    made a basic structure

---
+++ script.js
@@ -0,0 +1,23 @@
+var list = document.getElementById("list");
+
+
+function add() {
+    var text = document.getElementById("text").value;
+    if (text === "") {
+        alert("Please enter a task");
+    }
+    else {
+        var add = document.createElement("div");
+        add.classList.add("list-item");
+        add.innerHTML = `${text}
+        <button class='delete' onclick='del(this)' type="button">X</button>`;
+        document.getElementById("text").value = "";
+        list.appendChild(add);
+    }
+}
+
+function del(e) {
+    e.parentNode.remove();
+}
+
+
